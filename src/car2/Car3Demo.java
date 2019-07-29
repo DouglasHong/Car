75446db758ecfc45ae38a328c3699484a450fb16
dougl <dougl@DESKTOP-GETTEAK>
2019-07-29 12:52:14 -0700

    Improved the print statements and output

--- src/car2/Car3Demo.java
+++ src/car2/Car3Demo.java
@@ -15,32 +15,44 @@ public class Car3Demo {
         Speed speed = new Speed(5);
         Car3 car3 = new Car3(speed);
         int mySpeed = car3.currentSpeed();
-        System.out.println(mySpeed);
+        System.out.println("Speed: " + mySpeed);
 
         car3.inclementSpeed(10);
         mySpeed = car3.currentSpeed();
-        System.out.println(mySpeed);
+        System.out.println("Speed: " + mySpeed);
 
         car3.decreaseSpeed(9);
         mySpeed = car3.currentSpeed();
-        System.out.println(mySpeed);
+        System.out.println("Speed: " + mySpeed);
         calculateCitations(car3);
+        
         car3.multiplySpeed(12);
         mySpeed = car3.currentSpeed();
-        System.out.println(mySpeed);
+        System.out.println("Speed: " + mySpeed);
 
         car3.divideSpeed(15);
         mySpeed = car3.currentSpeed();
-        System.out.println(mySpeed);
+        System.out.println("Speed: " + mySpeed);
+        calculateCitations(car3);
+        
+        car3.inclementSpeed(120);
+        mySpeed = car3.currentSpeed();
+        System.out.println("Speed: " + mySpeed);
+        calculateCitations(car3);
+        
+        car3.inclementSpeed(120);
+        mySpeed = car3.currentSpeed();
+        System.out.println("Speed: " + mySpeed);
+        calculateCitations(car3);
 
     }
 
     private static void calculateCitations(Car3 car3) {
         Citation citations = car3.calculateCitation();
         if (citations.getTicket() == true) {
-            System.out.println("This ticket cost " + citations.getTicketCost());
+            System.out.println("Sorry, but you have to go back to driving school.");
         } else {
-            System.out.println("No ticket. You're a good driver");
+            System.out.println("No ticket bill. You're a good driver.");
         }
     }
 }
